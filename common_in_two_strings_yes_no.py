c9288878a224e4d1324515a3175b1b8bc25dded1
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-08-27 15:48:39 +0530

    Create common_in_two_strings_yes_no.py

---
+++ common_in_two_strings_yes_no.py
@@ -0,0 +1,26 @@
+MAX_CHAR = 26
+  
+
+def twoStrings(s1, s2) : 
+ 
+    v = [0] * (MAX_CHAR) 
+      
+
+    for i in range(len(s1)): 
+        v[ord(s1[i]) - ord('a')] = True
+      
+
+    for i in range(len(s2)) : 
+        if (v[ord(s2[i]) - ord('a')]) : 
+            return True
+      
+    return False
+  
+# Driver Code 
+if __name__ == "__main__":
+    str1,str2 = input().split()
+    
+    if (twoStrings(str1, str2)): 
+        print("yes") 
+    else: 
+        print("no") 
