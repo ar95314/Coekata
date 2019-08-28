ca7ea8b019e72d52fcc54674890e40b056c89be5
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-08-28 11:45:41 +0530

    Create asign_memory_program.cpp

---
+++ asign_memory_program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a=10;
+	int *a1=new int;
+	cout << a << a1;
+	delete all;
+	*a1=30;
+	cout <<*a1;
+	return 0;
+}
